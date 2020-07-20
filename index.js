e3bf491307a59fb032a8346e7376206efa4521b1
Kushagra Agarwal <kushagracool490@gmail.com>
2020-07-21 00:21:36 +0530

    Commit for v 0.1.0

---
+++ index.js
@@ -0,0 +1,25 @@
+// (function() {
+//     const containerDiv = document.querySelector('.click-info-container');
+//     containerDiv.style.display = 'none';
+//     const clickButton = document.querySelector('#click-button');
+//     clickButton.addEventListener('click',() => {
+//         if(containerDiv.style.display == 'none') {
+//             containerDiv.style.display = 'block';
+//         } else {
+//             containerDiv.style.display = 'none';
+//         }
+//     });
+// })();
+
+module.exports = (buttonRef , menuContainer) => {
+    menuContainer.style.display = 'none';
+    buttonRef.addEventListener('click',() => {
+        if(menuContainer.style.display == 'none') {
+            menuContainer.style.display = 'block';
+        } else {
+            menuContainer.style.display = 'none';
+        }
+    });
+}
+
+// export default addDropDownFunctionality;
